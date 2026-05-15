d5e7ec0b1757717bc5734b2c59745b7e959fb349
T-Jay Luke Gapusan <taji_0430@pc-58-0-100-190.cm.vtr.net>
2026-05-15 13:34:28 +0800

    Fixed fillable profile photo

--- app/Models/Resident.php
+++ app/Models/Resident.php
@@ -6,17 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resident extends Model
 {
-
     protected $fillable = [
 
-        'user_id',
         'full_name',
+        'contact_number',
         'age',
         'gender',
         'birthdate',
         'civil_status',
         'address',
         'barangay',
+        'profile_photo',
+        'resident_id_number',
 
     ];
 }
